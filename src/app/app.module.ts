a8706e8ad0343034dfbbdb3f027fe684e5ee6345
Darek <gawlikdaarek@gmail.com>
2023-04-05 15:54:40 +0200

    docs: added basic components and started with header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './Body/header/header.component';
+import { MainComponent } from './Body/main/main.component';
+import { AsideComponent } from './Body/aside/aside.component';
+import { FooterComponent } from './Body/footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HeaderComponent, MainComponent, AsideComponent, FooterComponent],
+  imports: [BrowserModule, NgbModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
