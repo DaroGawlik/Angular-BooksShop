567f2e8550e00f99b891a48cc4aaf8051f98ef8f
Darek <gawlikdaarek@gmail.com>
2023-04-09 08:54:07 +0200

    main: added books

--- src/app/Body/main/main.component.ts
+++ src/app/Body/main/main.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import books from '../../../books.json';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
 })
 export class MainComponent implements OnInit {
+  booksList: {
+    author: string;
+    imageLink: string;
+    title: string;
+    price: number;
+    description: string;
+  }[] = books;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
